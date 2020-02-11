aa16401486b50df0e85f4e8ebb2ac6d6fe9402a4
nakano <aiueo.candy3726@gmail.com>
2020-02-11 05:21:11 +0000

    パスワードカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_064558) do
+ActiveRecord::Schema.define(version: 2020_02_11_051924) do
 
   create_table "communities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 2020_02_08_064558) do
     t.text "artist"
     t.text "description"
     t.integer "prefecture", default: 0, null: false
+    t.string "password"
   end
 
 end
